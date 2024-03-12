00bdc87d0b996a564e0b7b1dd2bb975abc3bd1e4
lavakin <nikola@kalabova.eu>
2024-03-12 13:18:58 +0100

    new dist

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'SetMiGA',         
   packages = ['SetMiGA'],   
-  version = '0.1',      
+  version = '1.1',      
   license='MIT',       
   description = 'library designed to extract a minimal subset from a given set, optimizing a given (set of) objective(s). Based on the DEAP library.',   # Give a short description about your library
   author = 'Nikola Kalábová',              
