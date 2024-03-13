12d374ca2680d2aa454a564802fc07e7ce684a58
lavakin <nikola@kalabova.eu>
2024-03-13 11:07:05 +0100

    new setup

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
   description = 'library designed to extract a minimal subset from a given set, optimizing a given (set of) objective(s). Based on the DEAP library.',   # Give a short description about your library
   author = 'Nikola Kalábová',              
   author_email = 'nikola@kalabova.eu',     
-  url = 'https://github.com/lavakin/SetMiGA',  
-  download_url = 'https://github.com/lavakin/SetMiGA/archive/refs/tags/v1.1.tar.gz',    
+  url = 'https://github.com/lavakin/setmiga',  
+  download_url = 'https://github.com/lavakin/setmiga/archive/refs/tags/v0.1.tar.gz',    
   keywords = ['Genetic algorithms', 'minimal subset', 'multi-objective', "optimization"],   
   install_requires=[          
           'numpy',
