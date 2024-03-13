31134270a8b88cdb074dc38a7ad9e20bd819ec80
lavakin <nikola@kalabova.eu>
2024-03-13 11:16:44 +0100

    rename

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'setmiga',         
-  packages = ['setmiga'],   
-  version = '1.1',      
+  name = 'setminga',         
+  packages = ['setminga'],   
+  version = '0.1',      
   license='MIT',       
   description = 'library designed to extract a minimal subset from a given set, optimizing a given (set of) objective(s). Based on the DEAP library.',   # Give a short description about your library
   author = 'Nikola Kalábová',              
   author_email = 'nikola@kalabova.eu',     
   url = 'https://github.com/lavakin/setmiga',  
-  download_url = 'https://github.com/lavakin/setmiga/archive/refs/tags/v0.1.tar.gz',    
+  download_url = 'https://github.com/lavakin/setmiga/archive/refs/tags/v0.2.tar.gz',    
   keywords = ['Genetic algorithms', 'minimal subset', 'multi-objective', "optimization"],   
   install_requires=[          
           'numpy',
